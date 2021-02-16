e7a748da4e7ecab855655b7e29aee1f798b0d701
meghanvl <meghanvluoma@gmail.com>
2021-02-16 15:29:49 -0600

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -14,25 +14,55 @@ function displaySamples(data) {
     
     displaySamples(data.names);
 
+
     let sample_data = data.samples;
     let sample_sliced = sample_data.slice(0,10);
-    console.log(sample_sliced);
-    let otu_ids = sample_sliced[0].otu_ids;
+    let samples_reversed = sample_sliced.reverse();
+    let sample_values = samples_reversed;
+    console.log(sample_values);
+    let otu_ids = samples_reversed[0].otu_ids.slice(0,10);
     console.log(otu_ids);
-    let otu_labels = sample_sliced.otu_labels;
+    let otu_labels = samples_reversed[1].otu_labels.slice(0,10);
     console.log(otu_labels);
 
-    let trace1 = {
+    let barhChart = [{
         x: otu_ids,
         y: sample_values,
         text: otu_labels,
         type: "bar",
         orientation: "h"
-    };
+    }];
 
-    let barData = [trace1];
+    let layout = {
+    title: "Top 10 OTUs",
+    }
 
-    Plotly.newPlot("bar", barData);
+    Plotly.newPlot("bar", barhChart, layout);
+
+    let bubbleChart = [{
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: "markers",
+        marker: {
+            size: sample_values.length,
+            color: otu_ids
+        }
+    }];
+
+    let bubbleLayout = {
+        title: "Samples"
+    }
+
+    Plotly.newPlot("bubble", bubbleChart, bubbleLayout);
 
 });
+
+
+
+// d3.selectAll("#selDataset").on("change", getData);
+
+// function getData() {
+
+// }
         
