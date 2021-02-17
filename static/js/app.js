2cdf80a792ad2db6f2254a7d9daae8f77a745be0
meghanvl <meghanvluoma@gmail.com>
2021-02-17 16:12:39 -0600

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -1,33 +1,54 @@
+// async function sampleData() {
+//     let data = await d3.json("samples.json").then((data) => {
+//         console.log(data);
+//     });
+// }
+
+function handleChange() {
+    d3.event.preventDefault();
+    value = d3.seelct("#selDataset").node().value;
+    displaySamples(data);
+}
+
+
 d3.json("samples.json").then((data) => {
     console.log(data);
 
-dropdown = d3.select("#selDataset");
-// dropdown.append('option').text("This is a test")
-
-function displaySamples(data) {
+    function displaySamples(data) {
     // tbody.html("");
-    data.forEach((sample) => {
-        let cell = dropdown.append('option');
-        cell.text(sample);
-    });
-}
+        data.forEach((sample) => {
+            let dropdown = d3.select("#selDataset");
+            let cell = dropdown.append("option");
+            cell.text(sample);
+        });
+
+
+    }
     
     displaySamples(data.names);
 
+    // let sample_values = data.samples;
+    // let sample_sliced = sample_data.slice(0,10);
+    // console.log(sample_sliced);
 
-    let sample_data = data.samples;
-    let sample_sliced = sample_data.slice(0,10);
-    let samples_reversed = sample_sliced.reverse();
-    let sample_values = samples_reversed;
+    // let sample_values = sample_sliced;
+    // console.log(sample_values);
+
+    let sampleData = data.samples[0].sample_values.slice(0,10)
+    let sample_values = sampleData.sort((firstNum, secondNum) => firstNum - secondNum).reverse();
     console.log(sample_values);
-    let otu_ids = samples_reversed[0].otu_ids.slice(0,10);
+
+    let otuData = data.samples[0].otu_ids.slice(0,10);
+    let otu_ids = otuData.sort((firstNum, secondNum) => firstNum - secondNum).reverse();
     console.log(otu_ids);
-    let otu_labels = samples_reversed[1].otu_labels.slice(0,10);
+    
+    let otu_labels = data.samples[0].otu_labels.slice(0,10);
     console.log(otu_labels);
 
+    // horizontal bar chart
     let barhChart = [{
-        x: otu_ids,
-        y: sample_values,
+        x: sample_values,
+        y: otu_ids,
         text: otu_labels,
         type: "bar",
         orientation: "h"
@@ -39,6 +60,7 @@ function displaySamples(data) {
 
     Plotly.newPlot("bar", barhChart, layout);
 
+    // bubble chart
     let bubbleChart = [{
         x: otu_ids,
         y: sample_values,
@@ -58,11 +80,8 @@ function displaySamples(data) {
 
 });
 
+d3.selectAll("#selDataset").on("change", handleChange);
 
 
-// d3.selectAll("#selDataset").on("change", getData);
 
-// function getData() {
-
-// }
         
