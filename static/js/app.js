09017ed82af2c03cdc9b719ef4d48fa8a60eab59
meghanvl <meghanvluoma@gmail.com>
2021-02-23 13:55:10 -0600

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -43,7 +43,7 @@ function buildPlots(sampleID) {
                 b: 100
             },
             xaxis: {title: "Sample Values"},
-            width: 1050
+            width: 575
         };
     
         Plotly.newPlot("bar", barhChart, layout);
@@ -69,7 +69,72 @@ function buildPlots(sampleID) {
     
         Plotly.newPlot("bubble", bubbleChart, bubbleLayout);
 
-    });
+
+        // gauge chart
+        // const washFreq = data.metadata[0].wfreq;
+        // console.log(washFreq);
+
+        // const degrees = 180 - washFreq;
+	    // const radius = .5;
+        // const radians = degrees * Math.PI / 180;
+        // const x = radius * Math.cos(radians);
+        // const y = radius * Math.sin(radians);
+
+        // const mainPath = 'M -.0 -0.05 L .0 0.025 L ';
+	    // const pathX = String(x);
+	    // const space = ' ';
+	    // const pathY = String(y);
+	    // const pathEnd = ' Z';
+        // const path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+        // const gaugeChart = [{ 
+        //         type: 'scatter',
+        //         x: [0],
+        //         y:[0],
+	    //         marker: {size: 2, color:'850000'},
+	    //         showlegend: false,
+	    //         name: 'Frequency',
+	    //         text: washFreq,
+	    //         hoverinfo: 'text'},
+        //         { values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
+        //             rotation: 90,
+        //             text: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
+        //             textinfo: 'text',
+        //             textposition:'inside',	  
+        //             marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+		// 				 'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+		// 				 'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+		// 				 'rgba(255, 255, 255, 0)']},
+        //         labels: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
+        //         hoverinfo: 'label',
+        //             hole: .5,
+        //             type: 'pie',
+        //             showlegend: false
+        //         }];
+
+        // const gaugeLayout = {
+        //     shapes:[{
+        //         type: 'path',
+        //         path: path,
+        //         fillcolor: '850000',
+        //         line: {
+        //             color: '850000'
+        //         }
+        //     }],
+        //     title: '<b>Belly Button Washing Frequency</b> <br> Scrubs per Week',
+        //     height: 500,
+        //     width: 500,
+        //     xaxis: {zeroline:false, showticklabels:false,
+		// 	        showgrid: false, range: [-1, 1]
+        //     },
+        //     yaxis: {zeroline:false, showticklabels:false,
+		// 	        showgrid: false, range: [-1, 1]
+        //     }
+        // };
+
+        // Plotly.newPlot("gauge", gaugeChart, gaugeLayout);
+
+});
 
 }
 
